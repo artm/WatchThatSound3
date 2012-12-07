ba71540d236a44feba82c1112583b12b6995cc6a
Artem Baguinski <artm@v2.nl>
2012-12-07 20:24:20 +0100

    initial design for the editor

--- components/projecteditor/lib/ProjectEditor.cpp
+++ components/projecteditor/lib/ProjectEditor.cpp
@@ -3,7 +3,16 @@
 
 struct ProjectEditor::Detail
 {
+    void setup_tab_bar(ProjectEditor * self)
+    {
+        QTabBar * tabs = new QTabBar();
+        WidgetUtils::replace_widget( self, "tabs", tabs );
 
+        tabs->addTab("1 scenes");
+        tabs->addTab("2 tension line");
+        tabs->addTab("3 visual score");
+        tabs->addTab("4 audio edit");
+    }
 };
 
 ProjectEditor::ProjectEditor(QWidget *parent)
@@ -11,6 +20,7 @@ ProjectEditor::ProjectEditor(QWidget *parent)
 {
     setLayout( new QVBoxLayout );
     WidgetUtils::load_form(this,":/forms/ProjectEditor.ui");
+    detail->setup_tab_bar(this);
 }
 
 ProjectEditor::~ProjectEditor()
